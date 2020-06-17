652d6a2b59f3dab5f993aa73daa3af4445ad8ea0
vagrant <vagrant@localhost.localdomain>
2020-06-17 06:33:11 +0000

    Create Customer model by devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_16_123728) do
+ActiveRecord::Schema.define(version: 2020_06_17_061037) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -19,6 +18,26 @@ ActiveRecord::Schema.define(version: 2020_06_16_123728) do
     t.integer "quantity", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_ruby", null: false
+    t.string "first_name_ruby", null: false
+    t.string "tell_number", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
+    t.boolean "is_valid", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "destinations", force: :cascade do |t|
@@ -71,5 +90,4 @@ ActiveRecord::Schema.define(version: 2020_06_16_123728) do
     t.datetime "updated_at", null: false
   end
 
-
-
+end
