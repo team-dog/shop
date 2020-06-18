a83e3644a81058b1f5ee946cbc198561f7dada24
vagrant <vagrant@localhost.localdomain>
2020-06-18 14:05:56 +0000

    Add customers controller by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+   devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+  }
 
   devise_for :admins, controllers: {
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
     registrations: 'admin/registrations'
-  }
-  devise_for :customers, controllers: {
-    sessions:      'customers/sessions',
-    passwords:     'customers/passwords',
-    registrations: 'customers/registrations'
   }
 
 
