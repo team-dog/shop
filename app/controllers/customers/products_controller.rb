89f4a5e216adf0e94dc9d5ba7daea3a8de48afdd
vagrant <vagrant@localhost.localdomain>
2020-06-20 14:01:00 +0000

    Add products 一覧表示機能 詳細画面表示

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -5,5 +5,7 @@ class Customers::ProductsController < ApplicationController
 
 	def show
 		@product = Product.find(params[:id])
+		@cart_product_quantity = current_customer.cartproducts.new
 	end
+
 end
