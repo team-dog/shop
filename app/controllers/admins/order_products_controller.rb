0469401fa7f1161323a2295f43a0de532dd4dffd
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-06-23 16:25:22 +0900

    finish-admin-orders

--- app/controllers/admins/order_products_controller.rb
+++ app/controllers/admins/order_products_controller.rb
@@ -1,4 +1,13 @@
 class Admins::OrderProductsController < ApplicationController
+  # 注文ステータスの更新
   def update
+    status = OrderProduct.find(params[:order_id])
+    status.update(update_params)
+    redirect_to admins_order_path
   end
+
+  private 
+    def update_params
+        params.require(:order_product).permit(:status)
+    end
 end
