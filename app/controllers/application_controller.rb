f15d469ab9e117238adc404cd09d2f5a003bd214
vagrant <vagrant@localhost.localdomain>
2020-06-23 14:30:26 +0000

    Update admin root_pathに変更, ログイン・ログアウト後admin,customerそれぞれ別のリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,23 @@ class ApplicationController < ActionController::Base
 
 	protected
 
-	def after_sign_in_path_for(resource)
-		root_path
-	end
+	def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admins_root_path
+    else
+      root_path
+    end
+  end
 
-	#sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
-	def after_sign_out_path_for(resource)
-		root_path
+	#sign_out後のredirect先変更する。
+	def after_sign_out_path_for(resource_or_scope)
+		if resource_or_scope == :user
+			root_path
+		elsif resource_or_scope == :admin
+			new_admin_session_path
+		else
+			root_path
+		end
 	end
 
 	def configure_permitted_parameters
